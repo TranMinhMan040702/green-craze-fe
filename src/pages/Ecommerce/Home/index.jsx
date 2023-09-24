253ce79db6bf6fd89a529e66752bb3c1510076d0
Nguyễn Minh Sơn <nguyenminhson102002@gmail.com>
2023-09-24 10:49:06 +0700

    fix footer

--- src/pages/Ecommerce/Home/index.jsx
+++ src/pages/Ecommerce/Home/index.jsx
@@ -1,11 +1,7 @@
-import Header from '../../../layouts/Ecommerce/components/Header';
-import Footer from '../../../layouts/Ecommerce/components/Footer';
 function Home() {
     return (
         <>
-            <Header />
             <div></div>
-            <Footer />
         </>
     );
 }
