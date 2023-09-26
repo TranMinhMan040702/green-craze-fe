a5fb87eacaf1b566c6c53c51cd0662caeb65de08
Tran Man <man.tran@affina.com.vn>
2023-09-26 22:55:26 +0700

    fix merge

--- src/layouts/Ecommerce/EcommerceLayout/index.jsx
+++ src/layouts/Ecommerce/EcommerceLayout/index.jsx
@@ -1,7 +1,6 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import './ecommercelayout.scss';
-import Header from '../components/Header';
 
 function EcommerceLayout({ children }) {
     return (
