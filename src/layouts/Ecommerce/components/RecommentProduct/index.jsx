43e34b7302ce2964e511395fa3a4d2385f4a6e82
Tran Minh Man <mantm040702@gmail.com>
2024-05-23 00:30:23 +0700

    feature recommendations service

--- src/layouts/Ecommerce/components/RecommentProduct/index.jsx
+++ src/layouts/Ecommerce/components/RecommentProduct/index.jsx
@@ -2,9 +2,17 @@ import './recommentProduct.scss';
 import { Button } from 'antd';
 import CardProductRecomment from '../../components/CardProductRecomment';
 import { useGetListRecommendationByProduct } from '../../../../hooks/api';
+import { useEffect, useState } from 'react';
 
 function RecommentProduct({ productId }) {
     const { data, isLoading } = useGetListRecommendationByProduct(productId);
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        if (isLoading || !data) return;
+        setProducts(data?.data);
+    }, [isLoading, data]);
 
     return (
         <div className="recomment">
@@ -12,9 +20,13 @@ function RecommentProduct({ productId }) {
                 Có thể bạn sẽ thích
             </Button>
             <div className="card-list">
-                {data?.data?.items?.map((product) => {
+                {products.length > 0 &&
+                    products.map((item, index) => (
+                        <CardProductRecomment key={index} product={item} />
+                    ))}
+                {/* {data?.data?.items?.map((product) => {
                     return <CardProductRecomment key={product?.id} product={product} />;
-                })}
+                })} */}
             </div>
         </div>
     );
